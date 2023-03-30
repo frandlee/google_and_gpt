04b879b5692aaeac550a042294c014b666673a80
frandlee <frandlee@gmail.com>
2023-03-30 06:57:18 +0000

    update

--- readme.md
+++ readme.md
@@ -1,5 +1,19 @@
-How to run
-
+## How to run
+``` shell
 export GOOGLE_APPLICATION_CREDENTIALS=[your gcp service account credentials json file]
 export GPT_API_KEY=[your chatGpt key]
+export HOST_NAME=[your host name or ip]
+export PORT=[serive port]
+export CHAT_DIR=[directory for chatGpt results files]
 node ./src/index.js
+```
+
+## URLs
+1. Google translate
+[http://[hostName]:[port]/googleTrans?sentence=我有600日元。]
+
+2. Google TTS
+[http://[hostName]:[port]/googleTTS?sentence=どうぞ宜しくお願い致します。]
+
+3. ChatGpt chat api
+[http://[hostName]:[port]/chatgpt?words=降る]
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import { googleTTS } from './utils/voiceTool.js';
 import { translateToJp } from "./utils/translateTool.js";
 import { openaiChat } from "./utils/chatGpt.js";
-
 import express from 'express';
 
-const app = express();
-
+// Init
 const voiceFileDir = '/tmp';
-let GPT_API_KEY=process.env.GPT_API_KEY;
-console.debug(`GPT_API_KEY: ${GPT_API_KEY}`);
+let gpt_api_key = process.env.GPT_API_KEY;
+let hostName = process.env.HOST_NAME;
+let port = process.env.PORT;
+console.debug(`HOST_NAME: ${hostName}`);
+console.debug(`PORT: ${port}`);
+console.debug(`GPT_API_KEY: ${gpt_api_key}`);
+
+const app = express();
 
 app.post('/', function requestHandler(req, res) {
   console.debug(req.params);
@@ -21,7 +25,7 @@ app.get('/chatgpt', async function (req, res) {
   console.debug(req.query);
   const words = req.query['words'];
   console.debug('words: ' + words);
-  const results = await openaiChat(GPT_API_KEY, words)
+  const results = await openaiChat(gpt_api_key, words)
   console.debug(typeof results);
   console.debug(results);
   res.json(results)
@@ -38,8 +42,9 @@ app.get('/googleTrans', async function (req, res) {
   res.send();
 })
 
-// 1. download from GOOGLE TTS
-app.get('/google', async function (req, res) {
+// 2. download from GOOGLE TTS
+// 2.1 Generate Mp3
+app.get('/googleTTS', async function (req, res) {
   console.debug(req.query);
   const sentence = req.query['sentence'];
   console.debug('sentence: ' + sentence);
@@ -49,13 +54,13 @@ app.get('/google', async function (req, res) {
   console.debug('----------file generated!=-----');
   //googleTTS();
   //console.debug(req.headers);
-  const fileUrl = "http://192.168.10.11:8081/download?mp3=" + fileName;
+  const fileUrl = `http://${hostName}:${port}/download?mp3=${fileName}`;
   console.debug(fileUrl)
   res.json({ fileUrl: fileUrl })
   res.send();
 })
 
-//2. mp3 download
+//2.2 mp3 download
 app.get('/download', function (req, res) {
   //const __dirname = 'voicefiles'
   console.debug(req.query);
@@ -68,7 +73,7 @@ app.get('/download', function (req, res) {
 var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
-  console.log("Words service: http://%s:%s", host, port)
+  console.log(`Words service: http://${hostName}:${port}`)
 })
 
 
